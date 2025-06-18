7a8342c6b57cfe8474e1de4aca1fe67557b51e3d
Ragul-05 <ragulgopinath2005@gmail.com>
2025-06-18 14:41:46 +0530

    Added the folder and learned about the MVCPattern Example

---
+++ Design_Principles_and_Patterns/MVCPatternExample/Main.java
@@ -0,0 +1,19 @@
+package Design_Principles_and_Patterns.MVCPatternExample;
+
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("S101", "Ravi Kumar", "A");
+
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(student, view);
+
+        controller.updateView();
+
+        controller.setStudentName("Ravi Kumar R");
+        controller.setStudentGrade("A+");
+
+        controller.updateView();
+    }
+}
