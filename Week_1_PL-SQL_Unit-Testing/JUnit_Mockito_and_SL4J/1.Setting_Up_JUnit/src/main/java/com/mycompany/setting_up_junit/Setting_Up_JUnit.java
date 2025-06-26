ca9fd9ed110c594ab54b5b4314c239508b5e4395
Ragul-05 <ragulgopinath2005@gmail.com>
2025-06-26 21:52:11 +0530

    Adding the Folders for the Junit_MOkito and SL4J of the Task-1

---
+++ Week_1_PL-SQL_Unit-Testing/JUnit_Mockito_and_SL4J/1.Setting_Up_JUnit/src/main/java/com/mycompany/setting_up_junit/Setting_Up_JUnit.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.setting_up_junit;
+
+public class Setting_Up_JUnit {
+
+    public static void main(String[] args) {
+    }
+}
