2b30c4f12a5791ce9b016625ff357c9ef5a68834
Ragul-05 <ragulgopinath2005@gmail.com>
2025-06-28 21:06:38 +0530

    Adding the Folders for the Mokito of the Task-4

---
+++ Week_1_PL-SQL_Unit-Testing/JUnit_Mockito_and_SL4J/4.Mockito/src/main/java/com/mycompany/mockito/ExternalApi.java
@@ -0,0 +1,10 @@
+package com.mycompany.mockito;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public interface ExternalApi {
+    String getData();
+    void performAction();
+}
---
+++ Week_1_PL-SQL_Unit-Testing/JUnit_Mockito_and_SL4J/4.Mockito/src/main/java/com/mycompany/mockito/MyService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mockito;
+public class MyService {
+    private final ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+
+    public void doAction() {
+        api.performAction();
+    }
+}
