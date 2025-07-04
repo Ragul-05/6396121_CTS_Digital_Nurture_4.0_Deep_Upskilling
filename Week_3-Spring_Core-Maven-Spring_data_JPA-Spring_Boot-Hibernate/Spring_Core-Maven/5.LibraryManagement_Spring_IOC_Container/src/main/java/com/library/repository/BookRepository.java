8b6d3936184bc001d17b90450109fe12adf9031c
Ragul-05 <ragulgopinath2005@gmail.com>
2025-07-04 20:01:58 +0530

    Adding the week-3 assessment module for Spring core and Maven using Ioc Container

---
+++ Week_3-Spring_Core-Maven-Spring_data_JPA-Spring_Boot-Hibernate/Spring_Core-Maven/5.LibraryManagement_Spring_IOC_Container/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void loadBooks() {
+        System.out.println("BookRepository: Rendering books from database...");
+    }
+}
