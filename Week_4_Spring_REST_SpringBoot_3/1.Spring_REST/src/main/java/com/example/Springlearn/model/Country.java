d83fcd5f753e2788be3c0a2cbfc395c8c03d1b8c
Ragul-05 <ragulgopinath2005@gmail.com>
2025-07-14 14:39:31 +0530

    Adding the week-4 Assesment Excercise-1 to performing an Spring REST API by using an countries

--- Week_4_Spring_REST_SpringBoot_3/1.Spring_REST/src/main/java/com/example/Springlearn/model/Country.java
+++ Week_4_Spring_REST_SpringBoot_3/1.Spring_REST/src/main/java/com/example/Springlearn/model/Country.java
@@ -2,42 +2,49 @@ package com.example.Springlearn.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Entity
 public class Country {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Country.class);
 
     @Id
     private String code;
     private String name;
 
     public Country() {
-        System.out.println("Inside Country Constructor");
+        LOGGER.debug("Inside Country Constructor.");
+    }
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
     }
 
     public String getCode() {
+        LOGGER.debug("Getting code: {}", code);
         return code;
     }
 
     public void setCode(String code) {
-        System.out.println("Setting code");
+        LOGGER.debug("Setting code: {}", code);
         this.code = code;
     }
 
     public String getName() {
+        LOGGER.debug("Getting name: {}", name);
         return name;
     }
 
     public void setName(String name) {
-        System.out.println("Setting name");
+        LOGGER.debug("Setting name: {}", name);
         this.name = name;
     }
 
     @Override
     public String toString() {
-        return "Country{" +
-                "code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                '}';
+        return "Country {code='" + code + "', name='" + name + "'}";
     }
 }
-
--- Week_4_Spring_REST_SpringBoot_3/1.Spring_REST/src/main/resources/data.sql
+++ Week_4_Spring_REST_SpringBoot_3/1.Spring_REST/src/main/resources/data.sql
@@ -2,3 +2,4 @@ INSERT INTO country (code, name) VALUES ('IN', 'India');
 INSERT INTO country (code, name) VALUES ('US', 'United States');
 INSERT INTO country (code, name) VALUES ('DE', 'Germany');
 INSERT INTO country (code, name) VALUES ('JP', 'Japan');
+INSERT INTO country (code, name) VALUES ('FR', 'France');
